190c7ef3a91b01e63282f671fdf2ed171be18fac
hbfl3x <happybanda@dyuni.ac.mw>
2022-08-17 11:38:28 +0200

    change settings.py

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-by(7exv7^o&x62!tgjoy6j*^%x(f8_ya(-wiz4qa#&(yr@(sd&'
+with open('secret_key.json') as f:
+    key = json.load(f)
+
+SECRET_KEY = key["key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
